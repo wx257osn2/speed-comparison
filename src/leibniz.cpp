f8948c9a331c9efc467920c30a070f3ce9ca6920
I <1091761+wx257osn2@users.noreply.github.com>
2022-10-21 09:31:57 +0900

    unroll loop

--- src/leibniz.cpp
+++ src/leibniz.cpp
@@ -15,15 +15,10 @@ int main()
     }
     std::fclose(infile);                            // close file
     
-    rounds += 2u; // do this outside the loop
-    
     double pi = 1.0;
     
-    for (unsigned i=2u ; i < rounds ; ++i) // use ++i instead of i++
-    {
-        const double x = -1.0 + 2.0 * (i & 0x1); // allows vectorization
-        pi += (x / (2u * i - 1u)); // double / unsigned = double
-    }
+    for (unsigned i=1u ; i < rounds ; i += 4)
+        pi += -1. / (2u * i + 1u) + 1. / (2u * i + 3u) - 1. / (2u * i + 5u) + 1. / (2u * i + 7u);
     
     pi *= 4.;
     std::printf("%.16f\n", pi); // print 16 decimal digits of pi
